61f7f7889995e5dc670eec9501c176cd0e378036
Abood059 <ah0597414132@gmail.com>
2026-09-20 06:38:52 +0300

    chore: remove temp files, update .gitignore

--- patch_users_controller.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'src/modules/users/users.controller.ts';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace("import { formatUserResponse } from '../../common/utils/user-mapper.util';\n\nexport class UsersController", "export class UsersController");
-
-content = "import { formatUserResponse } from '../../common/utils/user-mapper.util';\n" + content;
-
-fs.writeFileSync(file, content);
--- test-prisma.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.userEducations.update({
-  where: { id: "123", userId: "456" },
-  data: { degree: "BS" }
-}).catch(() => {});
